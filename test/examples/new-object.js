7c96b5a69e43c1d2d149e89d028b5c1e153213b8
Brian Donovan <donovan@squareup.com>
2014-04-16 17:50:58 -0700

    Initial commit of project.

---
+++ test/examples/new-object.js
@@ -0,0 +1,3 @@
+/* jshint esnext:true */
+
+assert.deepEqual([1, 2, 3], new Array(...[1, 2, 3]));
---
+++ test/runner.js
@@ -0,0 +1,39 @@
+/**
+ * We pull in example files from test/examples/*.js. Write your assertions in
+ * the file alongside the ES6 spread "setup" code. The node `assert` library
+ * will already be in the context.
+ */
+
+Error.stackTraceLimit = 20;
+
+var esprima = require('esprima');
+var recast = require('recast');
+
+var es6spread = require('../lib');
+var es6restParams = require('es6-rest-params');
+
+var fs = require('fs');
+var path = require('path');
+var RESULTS = 'test/results';
+
+if (!fs.existsSync(RESULTS)) {
+  fs.mkdirSync(RESULTS);
+}
+
+require('example-runner').runCLI(process.argv.slice(2), {
+  transform: function(source, testName, filename) {
+    var recastOptions = {
+      esprima: esprima,
+      sourceFileName: filename,
+      sourceMapName: filename + '.map'
+    };
+
+    var ast = recast.parse(source, recastOptions);
+    ast = es6restParams.transform(es6spread.transform(ast));
+    var result = recast.print(ast, recastOptions);
+
+    fs.writeFileSync(path.join(RESULTS, testName + '.js'), result.code, 'utf8');
+    fs.writeFileSync(path.join(RESULTS, testName + '.js.map'), JSON.stringify(result.map), 'utf8');
+    return result.code;
+  }
+});
